6dbbdff79ceffd96c720c905d7cc6dc97d9b0ed6
berlinbot <96872607+berlinbot@users.noreply.github.com>
2022-01-20 20:27:45 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,16 +44,16 @@ def stats(update, context):
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
     stats = f'╭───《⟹ <b>Uncensored Playground Mirror ⟸</b>》\n│\n'\
-    f'├─<b>Bot Uptime:</b> {currentTime}\n\n'\
+    f'├─<b>Bot Uptime:</b> {currentTime}\n'\
             f'├─📀<b>Total Disk Space:</b> {total}\n'\
-            f'├─💽<b>Used:</b> {used} | <b>Free:</b> {free}\n\n'\
+            f'├─💽<b>Used:</b> {used} | <b>Free:</b> {free}\n'\
             f'├─📤<b>Upload:</b> {sent}\n'\
-            f'├─📥<b>Download:</b> {recv}\n\n'\
+            f'├─📥<b>Download:</b> {recv}\n'\
             f'├─💻<b>CPU:</b> {cpuUsage}%\n'\
             f'├─💾<b>RAM:</b> {mem_p}%\n'\
-            f'├─💿<b>DISK:</b> {disk}%\n\n'\
+            f'├─💿<b>DISK:</b> {disk}%\n'\
             f'├─♦️<b>Physical Cores:</b> {p_core}\n'\
-            f'├─♦️<b>Total Cores:</b> {t_core}\n\n'\
+            f'├─♦️<b>Total Cores:</b> {t_core}\n'\
             f'├─⚠️<b>SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
             f'├─💾<b>Memory Total:</b> {mem_t}\n'\
             f'├─<b>Memory Free:</b> {mem_a}\n'\
