ede2fedf360ec21d9d99a72a546ed5952c7a059f
sabbir404error1a <98693088+sabbir404error1a@users.noreply.github.com>
2022-02-04 17:06:12 +0600

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -237,7 +237,7 @@ class MirrorListener(listeners.MirrorListeners):
                 sendMessage(msg, self.bot, self.update)
             else:
                 chat_id = str(self.message.chat.id)[4:]
-                msg += f'\n╚—●<b>uploaded by: </b>{uname}\n\n'\Powered By 👉 @bangladeshhoarding'
+                msg += f'\n╚—●<b>uploaded by: </b>{uname}\n\n'
                 fmsg = ''
                 for index, item in enumerate(list(files), start=1):
                     msg_id = files[item]
